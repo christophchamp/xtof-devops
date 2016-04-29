fd24d57a1c78e333f8659ba82fd8ebf176e22f69
Christoph Champ <christoph.champ@gmail.com>
2016-04-28 18:15:57 -0700

    initial commit of cloud/os_get_azones.sh

---
+++ cloud/os_get_azones.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# AUTHOR: Christoph Champ <christoph.champ@gmail.com>
+# Requires jq 1.5+
+JQ=$(which jq)
+
+OS_AUTH_URL=http://1.2.3.4:5000/v2.0/
+OS_TENANT_NAME=admin
+OS_USERNAME=admin
+OS_PASSWORD=admin
+
+INFO=$(curl -sXPOST "${OS_AUTH_URL}/tokens" \
+        -H "Content-Type: application/json" \
+        -d "{\"auth\":{\"tenantName\":\"$OS_TENANT_NAME\",\"passwordCredentials\":\
+        {\"username\":\"$OS_USERNAME\",\"password\":\"$OS_PASSWORD\"}}}" | \
+        ${JQ} -crM '[.access.token.id + "," + (.access.serviceCatalog[] | select(.name == "nova") | .endpoints[].publicURL)] | .[]')
+
+TOKEN=${INFO%%,*}
+NOVA_ENDPOINT=${INFO#*,}
+
+IGNORE_ZONES="internal|nova"
+
+raw=$(curl -s -H "X-Auth-Token: ${TOKEN}" "${NOVA_ENDPOINT}/os-availability-zone/detail" | \
+    ${JQ} -crM '[.availabilityZoneInfo[].zoneName] | .[]' | \
+    grep -vE "(${IGNORE_ZONES})" | tr '\n' ',')
+
+IFS=',' read -r -a zones <<< "${raw%,}"
+
+for zone in "${zones[@]}"; do
+    raw=($(curl -s -H "X-Auth-Token: ${TOKEN}" "${NOVA_ENDPOINT}/os-availability-zone/detail" | \
+        ${JQ} --arg zone "$zone" '[.availabilityZoneInfo[] | select(.zoneName==$zone) | .hosts|keys] | .[]' | \
+        tr -d '[]",' | sed '/^$/d' | tr '\n' ',' | tr -d ' '))
+
+    IFS=',' read -r -a nodes <<< "${raw%,}"
+    for node in "${nodes[@]}"; do
+        echo "node: $zone $node"
+    done
+done
